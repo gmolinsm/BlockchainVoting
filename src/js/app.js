34f08e2d4e526429587d292af6495a7012928d84
Guillermo Molins <guimolins@gmail.com>
2021-05-05 00:48:31 +0200

    getJSON full path

--- src/js/app.js
+++ src/js/app.js
@@ -41,7 +41,7 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
+    $.getJSON("build/contracts/Election.json", function(election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
